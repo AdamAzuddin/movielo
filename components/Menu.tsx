177b1cde0ac2f9adae9e620e6c72e98582c62243
AdamAzuddin <05adamaz@gmail.com>
2023-04-02 19:06:04 +0800

    added necessary item in overlay menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -15,7 +15,7 @@ const Menu = () => {
       {/* hamburger icon */}
       <IoMdMenu size={32} style={{ color: "#FFFFFF" }} onClick={handleMenuToggle} />
       {isMenuOpen && (
-        <div style={{ position: 'absolute', top: '50px', backgroundColor: '#FFFFFF', width: '200px', padding: '16px' }}>
+        <div style={{ position: 'absolute', backgroundColor: '#0a0a0a', width: '200px',padding:'16px'}}>
           <OverlayMenu/>
         </div>
       )}
--- components/OverlayMenu.tsx
+++ components/OverlayMenu.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const OverlayMenu = () => {
   return (
-    <div>OverlayMenu</div>
+    <div>
+      <h2 style={{color:'silver'}}>My Account</h2>
+      <h2 style={{color:'silver'}}>Username</h2>
+      <h2 style={{color:'silver'}}>Genre</h2>
+      <h2 style={{color:'silver'}}>Movies</h2>
+      <h2 style={{color:'silver'}}>Series</h2>
+      <h2 style={{color:'silver'}}>My List</h2>
+      <h2 style={{color:'silver'}}>About</h2>
+      <h2 style={{color:'silver'}}>Log Out</h2>
+    </div>
+    
   )
 }
 
