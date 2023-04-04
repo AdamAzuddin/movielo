16fe7226c35b828d65ebea5fb13cfec7dd2653df
AdamAzuddin <05adamaz@gmail.com>
2023-04-04 21:39:17 +0800

    rearranged OverlayMenu list

--- components/OverlayMenu.tsx
+++ components/OverlayMenu.tsx
@@ -1,48 +1,55 @@
 import React from "react";
 import Link from "next/link";
 
+const data = [
+  {
+    name: "My Account",
+    Link: "../pages/MyProfile",
+  },
+  {
+    name: "Genre",
+    Link: "/../pages/Genre",
+  },
+  {
+    name: "Movies",
+    Link: "/../pages/Movies",
+  },
+  {
+    name: "Series",
+    Link: "/../pages/Series",
+  },
+  {
+    name: "My List",
+    Link: "/../pages/MyList",
+  },
+  {
+    name: "About",
+    Link: "/../pages/About",
+  },
+  {
+    name: "Log Out",
+    Link: "/../pages/SignIn",
+  },
+];
+
 const OverlayMenu = (props: any) => {
 
   const [active, setActive] = props.activeState
   return (
     <>
-      <div  style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
-        <Link style={{ color: "white", paddingLeft:"15px" }} href="../pages/MyProfile" onClick={() => setActive(false)}>
-          My Account
-        </Link>
-      </div>
-      <div  style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
-        <Link style={{ color: "white", paddingLeft:"15px" }} href="../pages/MyProfile" onClick={() => setActive(false)}>
-          Username
-        </Link>
-      </div>
-      <div  style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
-        <Link style={{ color: "white", paddingLeft:"15px" }} href="../pages/Genre" onClick={() => setActive(false)}>
-          Genre
-        </Link>
-      </div>
-      <div  style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
-        <Link style={{ color: "white", paddingLeft:"15px" }} href="../pages/Movies" onClick={() => setActive(false)}>
-          Movies
-        </Link>
-      </div>
-      <div  style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
-        <Link style={{ color: "white", paddingLeft:"15px" }} href="../pages/Series" onClick={() => setActive(false)}>
-          Series
-        </Link>
-      </div>
-      <div  style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
-        <Link style={{ color: "white", paddingLeft:"15px" }} href="../pages/MyList" onClick={() => setActive(false)}>
-          My List
-        </Link>
-      </div>
-      <div  style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
-        <Link style={{ color: "white", paddingLeft:"15px" }} href="../pages/About" onClick={() => setActive(false)}>
-          About
-        </Link>
-      </div>
-      <div style={{ color: "white", paddingLeft:"15px" }}>Log Out</div>
-    </>
+      <ul style={{ listStyleType: "none" }}>
+          {data.map((item, i) => (
+            <li key={i} style={{padding: "4vh 0", borderBottom: "0.5px solid white"}}>
+              <Link
+                href={item.Link}
+                onClick={() => setActive(false)}
+                style={{ color: "white", paddingLeft:"15px" }}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>    </>
   );
 };
 
