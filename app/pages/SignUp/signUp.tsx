bb0cf5c63463d752f70a2674a8443d286be927d0
AdamAzuddin <05adamaz@gmail.com>
2023-04-05 16:03:18 +0800

    added ui to sign up page

--- app/pages/SignUp/signUp.tsx
+++ app/pages/SignUp/signUp.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { TextField } from "@mui/material";
+import { Button } from "@material-ui/core";
 
 const SignUpComponent = () => {
-  return <div style={{ color: "white" }}>Sign up</div>;
+  return <div>
+  <h1>Log in to your account</h1>
+  <h2>
+    Discover thousands of movies and series <br/> from popular streaming services
+    <br/> all at one place
+  </h2>
+  <div>
+  <TextField
+      label="Username"
+      required={true}
+      variant="filled"
+      margin="dense"
+    />
+  </div>
+  <div>
+  <TextField
+      label="Password"
+      required={true}
+      variant="filled"
+      type="password"
+      autoComplete="current-password"
+      margin="dense"
+    />
+  </div>
+
+  <div>
+  <TextField
+      label="Confirm Password"
+      required={true}
+      variant="filled"
+      type="password"
+      autoComplete="current-password"
+      margin="dense"
+    />
+  </div>
+
+    
+    
+
+    <div style={{margin:"40px"}}>
+      <Button variant="contained" color="primary">
+        Sign up
+      </Button>
+    </div>
+</div>;
 };
 
 export default SignUpComponent;
